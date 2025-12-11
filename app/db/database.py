de5271412fc682656093efbfab674517230d9500
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2025-12-11 17:04:33 +0530

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://admin:password@localhost:5432/agentforge_db"
+DATABASE_URL = "postgresql://agentforge_user:agentforge_pass@postgres:5432/agentforge_db"
 
 engine = create_engine(DATABASE_URL)
 
